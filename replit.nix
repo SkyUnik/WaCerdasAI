f23ab8de14be8878ef77b628eeec4e79da33edbe
SkyUnik <111160287+SkyUnik@users.noreply.github.com>
2023-07-22 13:36:33 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,10 @@
 { pkgs }: {
-    deps = [
-    pkgs.nodejs_20
-    ];
+	deps = [
+		pkgs.nano
+        pkgs.nodejs-18_x
+        pkgs.yarn
+        pkgs.python311
+        pkgs.python311Packages.pip
+        pkgs.conda
+	];
 }
